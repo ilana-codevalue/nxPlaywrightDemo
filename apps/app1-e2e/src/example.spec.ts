972a68f1bd2dd06557d2f27005840a2f3ff52354
ilan.adasha <ilana@codevalue.com>
2024-08-21 19:12:14 +0300

    adding e2e test

--- apps/app1-e2e/src/example.spec.ts
+++ apps/app1-e2e/src/example.spec.ts
@@ -6,3 +6,10 @@ test('has title', async ({ page }) => {
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
 });
+
+test('has Lib1', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect Lib1 to contain a substring.
+  expect(await page.locator('#root > div > div:nth-child(6) > ul > li:nth-child(2) > a').innerText()).toContain('Lib1');
+});
--- apps/app2-e2e/src/example.spec.ts
+++ apps/app2-e2e/src/example.spec.ts
@@ -6,3 +6,12 @@ test('has title', async ({ page }) => {
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
 });
+
+
+test('has Lib2', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect Lib1 to contain a substring.
+  expect(await page.locator('#root > div > div:nth-child(9) > ul > li:nth-child(3) > a').innerText()).toContain('Lib2');
+});
+
