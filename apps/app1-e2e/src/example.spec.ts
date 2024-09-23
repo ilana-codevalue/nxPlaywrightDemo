3c0e6c9ee84d2fc2a359cb4427d23574c74d6d57
ilan.adasha <ilana@codevalue.com>
2024-09-23 17:38:05 +0300

    ci - shard, workers, parallel

--- apps/app1-e2e/src/example.spec.ts
+++ apps/app1-e2e/src/example.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-// test.describe.configure({ mode: 'parallel' });
+test.describe.configure({ mode: 'parallel' });
 
 test.beforeEach('', async ({ page }) => {
   await page.goto('http://localhost:4200');
 });
 
-test('has title', async ({ page }) => {
+test.describe('app1 tests', () => {
+  test('has title', async ({ page }) => {
+    // Expect h1 to contain a substring.
+    expect(await page.locator('h1').innerText()).toContain('Welcome');
+  });
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
-});
-
-test('Lib1 visible', async ({ page }) => {
-
-  // Expect Lib1 to contain a substring.
-  expect(await page.getByRole('link', { name: 'Lib1' }).isVisible()).toBe(true);
+  test('Lib1 visible', async ({ page }) => {
+    // Expect Lib1 to contain a substring.
+    expect(await page.getByRole('link', { name: 'Lib1' }).isVisible()).toBe(
+      true
+    );
+  });
 });
