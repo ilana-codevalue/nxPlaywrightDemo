6594f80b1656fd01bceb9afc36e35a0f6a15efe4
ilan.adasha <ilana@codevalue.com>
2024-09-25 14:18:50 +0300

    ci-nx-affected - 8

--- libs/lib2/src/lib/lib2.tsx
+++ libs/lib2/src/lib/lib2.tsx
@@ -3,7 +3,7 @@ import styles from './lib2.module.css';
 export function Lib2() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Lib2! - update5</h1>
+      <h1>Welcome to Lib2! - update6</h1>
     </div>
   );
 }
