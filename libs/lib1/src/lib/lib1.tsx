dbe1d015171590f47ecd1f9bce189e5008fcefd0
ilan.adasha <ilana@codevalue.com>
2024-09-27 16:46:54 +0300

    update

--- libs/lib1/src/lib/lib1.tsx
+++ libs/lib1/src/lib/lib1.tsx
@@ -3,7 +3,7 @@ import styles from './lib1.module.css';
 export function Lib1() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Lib1!</h1>
+      <h1>Welcome to Lib1!!</h1>
     </div>
   );
 }
