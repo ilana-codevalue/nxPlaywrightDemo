87ce7c5dea8e3767e5ee11e9b178a24e5da88032
ilan.adasha <ilana@codevalue.com>
2024-09-27 16:12:01 +0300

    minor

--- apps/app1-e2e/src/example.spec.ts
+++ apps/app1-e2e/src/example.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.configure({ mode: 'parallel' });
-
 test.describe('app1 tests', () => {
   
   test.beforeEach('', async ({ page }) => {
